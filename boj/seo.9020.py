150286871bc6eedaf8f259d6d79ae4500adcd1ab
HyeonSeuk <seo5719@naver.com>
2023-08-15 16:57:48 +0900

    modify seo.9020

--- boj/seo.9020.py
+++ boj/seo.9020.py
@@ -1,8 +1,10 @@
+import math
+
 def prime(n):
     if n == 2: # 2는 소수에 포함 되므로 True로
         return True
     else:
-        for i in range(2, int(n**0.5)+1): # 2부터 n의 제곱근까지의 수로 n을 나누어 0인지 확인
+        for i in range(2, int(math.sqrt(n))+1): # 2부터 n의 제곱근까지의 수로 n을 나누어 0인지 확인
             if n % i == 0: # 나누어 떨어지는 경우 소수가 아니므로 False 반환
                 return False
         return True # 반복문의 모두 통과 시 소수이므로 True 반환
